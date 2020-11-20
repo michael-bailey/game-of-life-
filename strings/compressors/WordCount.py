d4b7b8e93fc8afb556b5e3a9ad08f7c43c8dd205
michael-bailey <mickyb18a@gmail.com>
2020-11-20 08:34:49 +0000

    fixed the mapping creation.

--- strings/compressors/WordCount.py
+++ strings/compressors/WordCount.py
@@ -1,8 +1,11 @@
+import json as js
+
+
 class CountCompressor:
     # generate initial count
     def __init__(self, text=""):
         print(" -- init --")
-        self.segments = [i for i in getwords(text)]
+        self.text = text
         self.mapping = {}
         self.reverseMapping = {}
 
@@ -10,7 +13,7 @@ class CountCompressor:
         count = {}
 
         # create count store for each word
-        for i in self.segments:
+        for i in getwords(self.text):
             print(i)
             print("key exists", i in count)
             print("is alphanum", i.isalpha() or i.isnumeric())
@@ -26,15 +29,20 @@ class CountCompressor:
                 print("new: ", i, count[i])
                 continue
 
-        count_pair.append([(count[i], i) for i in count.keys()])
+            self.mapping[i] = i
+            self.reverseMapping[i] = i
+
+        count_pair += [(count[i], i) for i in count.keys()]
         count_pair.sort()
 
-        self.mapping = {count_pair[i][1]: i for i in range(len(count_pair))}
-        self.reverseMapping = {i: count_pair[i][1] for i in range(len(count_pair))}
+        print(count_pair)
+
+        self.mapping.update({count_pair[i][1]: i for i in range(len(count_pair))})
+        self.reverseMapping.update({i: count_pair[i][1] for i in range(len(count_pair))})
 
     def update(self, text=""):
         print(" -- update --")
-        self.segments += [i for i in getwords(text)]
+        self.text += text
         self.mapping = {}
         self.reverseMapping = {}
 
@@ -42,7 +50,7 @@ class CountCompressor:
         count = {}
 
         # create count store for each word
-        for i in self.segments:
+        for i in getwords(self.text):
             print(i)
             print("key exists", i in count)
             print("is alphanum", i.isalpha() or i.isnumeric())
@@ -58,14 +66,22 @@ class CountCompressor:
                 print("new: ", i, count[i])
                 continue
 
-        count_pair.append([(count[i], i) for i in count.keys()])
-        count_pair.sort()
+            self.mapping[i] = i
+            self.reverseMapping[i] = i
 
-        self.reverseMapping = {count_pair[i][1]: i for i in range(len(count_pair))}
-        self.mapping = {i: count_pair[i][1] for i in range(len(count_pair))}
+        count_pair += [(count[i], i) for i in count.keys()]
+        count_pair.sort(reverse=True)
 
-    def gettext(self) -> str:
-        return ''.join(self.reverseMapping[i] for i in self.segments)
+        print("count: ", count_pair)
+
+        self.mapping.update({count_pair[i][1]: i for i in range(len(count_pair))})
+        self.reverseMapping.update({i: count_pair[i][1] for i in range(len(count_pair))})
+
+    def compress(self) -> str:
+        return ''.join(str(self.mapping[i]) for i in getwords(self.text))
+
+    def gettext(self):
+        return self.text
 
     def getmapping(self) -> dict:
         return self.mapping
@@ -87,8 +103,21 @@ def getwords(text=""):
     yield word
 
 if __name__ == '__main__':
-    a = CountCompressor("a a a a b b b b c c s s d d d")
-    a.update("zz z zz z zz zz zzz zz z z")
-    print(a.getmapping())
-    print(a.getreversemapping())
-    print(a.gettext())
+    a = CountCompressor("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum convallis arcu rhoncus purus porttitor, id vestibulum lacus ullamcorper. Curabitur vel tempus magna, et volutpat turpis. Phasellus aliquam commodo libero sit amet volutpat. Nulla libero mauris, commodo ut mi id, porttitor vestibulum sapien. Suspendisse eu tortor ut urna tincidunt laoreet non ut magna. Proin id scelerisque tortor, id luctus nibh. Sed laoreet massa leo. Duis eu erat porta, consequat metus in, condimentum erat. Nulla fringilla nibh eget velit vulputate, eget congue dui vulputate. Curabitur sagittis velit in vehicula lobortis. Sed eget ultrices eros, et mollis neque. Cras ac facilisis nulla. In ultrices scelerisque diam quis finibus. Ut at urna mattis, vestibulum purus ac, viverra dui. Sed viverra magna lorem.
+
+Nunc consequat magna aliquet elit malesuada, eget rhoncus magna laoreet. Nulla finibus luctus vulputate. Aenean ultricies in dolor vitae malesuada. Mauris iaculis massa ligula, ac finibus nisi dignissim vel. Integer euismod orci sit amet augue vulputate, vitae consequat lacus pellentesque. Mauris pellentesque congue orci, non efficitur nulla posuere in. Ut sapien velit, finibus ac tristique nec, vulputate a augue. Phasellus at vestibulum lectus. Donec cursus ac sapien quis placerat. Sed vel laoreet ante. Vivamus et ante ac lacus venenatis sagittis. Vivamus bibendum viverra ipsum ac ullamcorper.
+
+Mauris ornare congue interdum. Etiam tempor vitae urna quis dapibus. Sed lacinia ultricies eros, in vehicula ex efficitur vitae. Fusce iaculis neque quis pharetra imperdiet. Maecenas faucibus leo nec aliquam tristique. Quisque euismod, est in laoreet pharetra, velit nisi malesuada turpis, et auctor lacus odio eu ipsum. Fusce eu dolor ac nulla fermentum interdum et at augue.
+
+Integer sollicitudin eget nisi quis accumsan. Nulla at pharetra nulla. Duis consequat viverra sem, ac ultrices metus venenatis nec. Etiam ipsum nulla, pulvinar eget mi in, ultricies fringilla dui. Vestibulum in aliquet ligula. Integer sit amet sem sit amet ex porttitor condimentum. Suspendisse at nisi vulputate nulla blandit fermentum. Donec eu pretium arcu. Phasellus semper pretium erat.
+
+Fusce sed convallis lorem. Suspendisse dictum est et justo maximus venenatis in sit amet ligula. Integer bibendum pellentesque nibh id interdum. Curabitur rutrum sapien ac condimentum tempus. Nunc tempor mattis odio vitae condimentum. Cras dapibus quam metus, at rhoncus orci vestibulum vitae. Pellentesque volutpat sem ut aliquet dictum. Donec eget dui sem. Nunc accumsan sodales magna sit amet pulvinar. Praesent odio velit, aliquet in metus non, commodo faucibus tellus. Nullam scelerisque consectetur urna, non ornare nunc. Integer imperdiet, nibh eu vehicula posuere, quam nisi vestibulum lorem, varius fringilla lectus eros sit amet lectus.""")
+
+    print("text: ", a.gettext())
+    print("mapping: ", a.getmapping())
+    print("reverse mapping: ", a.getreversemapping())
+    print(a.compress())
+
+    open("compressed.txt", "w").write(a.compress())
+    js.dump(a.mapping, open("mapping.json", "w"))
+    js.dump(a.reverseMapping, open("reverseMapping.json", "w"))
