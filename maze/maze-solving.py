436ac9c6a0cee3818d635e68629b4a7f09b554f1
michael-bailey <mickyb18a@gmail.com>
2020-12-31 16:52:17 +0000

    added new solver to the main file

--- maze/maze-solving.py
+++ maze/maze-solving.py
@@ -1,3 +1,4 @@
+from classes.Node import DijkstraSolver
 from pprint import pprint
 from PIL.Image import fromarray
 import numpy as np
@@ -7,11 +8,12 @@ import sys
 sys.setrecursionlimit(100000)
 
 if __name__ == "__main__":
-    #maze = Maze("./maze.png", output_image=True, threaded=False)
+    # maze = Maze("./maze.png", output_image=True, threaded=False)
     #maze = Maze("./maze.png", output_image=True, threaded=True)
 
-    #maze = Maze("./perfect2k.png", threaded=False)
-    maze = Maze("./perfect15k.png",  threaded=False)
+    # maze = Maze("./perfect2k.png", threaded=False)
+    maze: Maze = Maze("./perfect15k.png",  threaded=False)
 
-    maze.solve_dikstra(recursive = False)
+    # maze.solve_dikstra(recursive = False)
+    maze.solve(DijkstraSolver())
     print("done")
