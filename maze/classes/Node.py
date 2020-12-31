35fc2a93e264242db0678de7db40acc150600dfd
michael-bailey <mickyb18a@gmail.com>
2020-12-31 09:57:53 +0000

    added getNodes abstract method

--- maze/classes/Node.py
+++ maze/classes/Node.py
@@ -6,7 +6,7 @@ import uuid
 
 class INode(object):
 
-	# list of all nodes of the class
+    # list of all nodes of the class
 	nodes: list[INode] = []
 
 	@staticmethod
@@ -29,51 +29,52 @@ class INode(object):
 	def addNode(node: INode):
 		INode.nodes.append(node)
 
+	@abstractmethod
+	def getNodes(self) -> list[INode]:
+		raise NotImplementedError()
+
 	def accept(self, solver: ISolver):
 		solver.resolve(self)
 
 class Node(INode):
 
-    def __init__(self, pos_x, pos_y, type=1):
-        self.id = uuid.uuid4()
+	def __init__(self, pos_x, pos_y, type=1):
+		self.id = uuid.uuid4()
 
-        self.distance = None
-        self.type = 0
+		self.distance = None
+		self.type = 0
 
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+		self.pos_x = pos_x
+		self.pos_y = pos_y
 
-        self.previous = None
+		self.previous = None
 
-        self.nodes = []
-        Node.nodes.append(self)
+		self.nodes = []
+		Node.nodes.append(self)
 
-    def addNode(self, node):
-        self.nodes.append(node)
-
-    def pathFind(self, recursive = True):
-        if recursive:
-            if self.previous == None: return [self]
-            return self.previous.pathFind(recursive=True) + [self]
+	def pathFind(self, recursive = True):
+		if recursive:
+			if self.previous == None: return [self]
+			return self.previous.pathFind(recursive=True) + [self]
         
-        nodebacktrack = []
-        for i in backtrack(self):
-            nodebacktrack.append(i)
+		nodebacktrack = []
+		for i in backtrack(self):
+			nodebacktrack.append(i)
 
-        return nodebacktrack
+		return nodebacktrack
 
 
-    def __str__(self):
-        return "node uuid: {} x: {} y:{} distance:{}".format(self.id, self.pos_x, self.pos_y, self.distance)
+	def __str__(self):
+		return "node uuid: {} x: {} y:{} distance:{}".format(self.id, self.pos_x, self.pos_y, self.distance)
 
-    def __lt__(self, other):
-        return self.distance < other.distance
+	def __lt__(self, other):
+		return self.distance < other.distance
 
-    def __gt__(self, other):
-        return self.distance > other.distance
+	def __gt__(self, other):
+		return self.distance > other.distance
 
 def backtrack(node):
-    current = node
-    while current.previous != None:
-        yield current
-        current = current.previous
+	current = node
+	while current.previous != None:
+		yield current
+		current = current.previous
