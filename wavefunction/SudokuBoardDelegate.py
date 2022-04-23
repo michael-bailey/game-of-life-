551e53f24868b6ec5f214b13d1c729fff6c763c2
michael-bailey <mickyb18a@gmail.com>
2022-04-24 00:50:38 +0100

    added extra init pdate method to delegate

--- wavefunction/SudokuBoardDelegate.py
+++ wavefunction/SudokuBoardDelegate.py
@@ -10,3 +10,7 @@ class SudokuBoardDelegate(object):
 	@abstractmethod
 	def cell_update(self, board: 'SudokuBoard.SudokuBoard', row: int, col: int):
 		raise NotImplementedError()
+
+	@abstractmethod
+	def init_update(self, board: 'SudokuBoard.SudokuBoard', row: int, col: int):
+		raise NotImplementedError()
