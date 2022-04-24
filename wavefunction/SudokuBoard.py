d2a0f5930cc943ea4d62863df5a7e1a57bc5f20f
michael-bailey <mickyb18a@gmail.com>
2022-04-24 14:29:23 +0100

    added chunking algorithm

--- wavefunction/SudokuBoard.py
+++ wavefunction/SudokuBoard.py
@@ -16,36 +16,36 @@ class SudokuBoard(object):
 		This does not include the wave function probabilities.
 	'''
 
-	__array: List[List[str]]
+	array: List[List[str]]
 	__delegate: Optional['SudokuBoardDelegate.SudokuBoardDelegate']
 	
 	def __init__(self) -> None:
-		self.__array = [['_' for _ in range(0,10)] for _ in range(0,10)]
+		self.array = [['_' for _ in range(0,10)] for _ in range(0,10)]
 
 	def __str__(self) -> str:
-		return '\n'.join([' '.join([str(cell) for cell in row]) for row in self.__array])
+		return '\n'.join([' '.join([str(cell) for cell in row]) for row in self.array])
 
 	def set_delegate(self, delegate: 'SudokuBoardDelegate.SudokuBoardDelegate'):
 		self.__delegate = delegate
 		self.__delegate.init_update(self)
 
 	def get_row(self, index: int) -> List[str]:
-		return self.__array[index]
+		return self.array[index]
 
 	def get_col(self, index: int) -> List[str]:
-		return [x[index] for x in self.__array]
+		return [x[index] for x in self.array]
 
 	def get_square(self, col: int, row: int) -> List[List[str]]:
 		col_index = 0 + (3*col)
 		row_index = 0 + (3*row)
-		return [self.__array[i][row_index:row_index+3] for i in range(col_index, col_index+3)]
+		return [self.array[i][row_index:row_index+3] for i in range(col_index, col_index+3)]
 
 	def get_posibilities(self, row: int, col: int) -> Optional[Set[str]]:
 		if self.__delegate == None: return None
 		return self.__delegate.get_posibilities(row,col)
 	
 	def set_number(self, row: int, col: int, number: str):
-		self.__array[row][col] = number
+		self.array[row][col] = number
 
 	@staticmethod
 	def load(filename: str) -> SudokuBoard:
@@ -53,6 +53,6 @@ class SudokuBoard(object):
 		with open(filename, 'r') as file:
 			arr = [x for x in reader(file)]
 			board = SudokuBoard()
-			board.__array = arr
+			board.array = arr
 			return board
 		
--- wavefunction/SudokuWaveFuncitonBoard.py
+++ wavefunction/SudokuWaveFuncitonBoard.py
@@ -2,7 +2,7 @@
 	SudokuWaveFuncitonBoard.py
 '''
 
-from typing import List
+from typing import Any, List
 import SudokuBoard
 import SudokuBoardDelegate
 import SudokuCellSuperposition
@@ -12,16 +12,26 @@ class SudokuWaveFuncitonBoard(SudokuBoardDelegate.SudokuBoardDelegate):
 
 	__array: List[List['SudokuCellSuperposition.SudokuCellSuperposition']]
 
+	__rows: List[List['SudokuCellSuperposition.SudokuCellSuperposition']]
+	__cols: List[List['SudokuCellSuperposition.SudokuCellSuperposition']]
+	__squares: List[List['SudokuCellSuperposition.SudokuCellSuperposition']]
+
 	def __init__(self) -> None:
 		super().__init__()
+		# create array for holding all values
 		self.__array = [[SudokuCellSuperposition.SudokuCellSuperposition() for _ in range(0,10)] for _ in range(0,10)]
+
+		# create arrays for each
+		self.__rows = [x for x in self.__array]
+		self.__cols = [[i[row] for i in self.__array] for row in range(0, 9)]
+		self.__squares = chunk(self.__array)
 
 	def cell_update(self, board: 'SudokuBoard.SudokuBoard', row: int, col: int):
 		print("[SudokuWaveFuncitonBoard:cell_update] row:{} col:{}", row,col)
 
 	def init_update(self, board: 'SudokuBoard.SudokuBoard'):
 		print("[SudokuWaveFuncitonBoard:init_update]")
-		for (r_index, row) in enumerate(board.__array):
+		for (r_index, row) in enumerate(board.array):
 			for (c_index, col) in enumerate(row):
 				match col:
 					case "_":
@@ -31,5 +41,17 @@ class SudokuWaveFuncitonBoard(SudokuBoardDelegate.SudokuBoardDelegate):
 						
 	def collapse(self, row: int, col: int, number: str):
 		self.__array[row][col].collapse(number)
-		raise NotImplementedError()
-	
+
+
+def chunk(arr: List[List[Any]]) -> List[List[Any]]:
+	chunks = [[row[i:i + 3] for i in range(0, len(row), 3)] for row in arr]
+	final = []
+
+	for i in range(0,len(chunks), 3):
+		cur = [[],[],[]]
+		for j in chunks[i:i+3]:
+			cur[0].extend(j[0])
+			cur[1].extend(j[1])
+			cur[2].extend(j[2])
+		final.extend(cur)
+	return final
